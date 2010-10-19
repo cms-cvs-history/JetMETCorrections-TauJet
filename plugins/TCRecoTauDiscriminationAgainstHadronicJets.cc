07dbbd5f6a4ce03861b70c89a19c7ef3192f1af6
cmsbuild <>
2010-10-19 22:25:38 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- TauJet-CMSSW_5_0_0_pre5_root532rc1'.
    Sprout from master 2010-09-12 08:34:16 UTC Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch> 'Commented function TCTauCorrector::correction(const reco::CaloJet& fJet)'
    Cherrypick from master 2010-10-19 22:22:26 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Remove 'using' directives from headers':
        plugins/TCRecoTauDiscriminationAgainstHadronicJets.cc
        plugins/TCRecoTauDiscriminationAlgoComponent.cc
        test/TCTauAnalysis.cpp

--- plugins/TCRecoTauDiscriminationAgainstHadronicJets.cc
+++ plugins/TCRecoTauDiscriminationAgainstHadronicJets.cc
@@ -8,20 +8,22 @@
 #include "RecoTauTag/RecoTau/interface/TauDiscriminationProducerBase.h"
 #include "RecoTauTag/RecoTau/interface/TCTauAlgorithm.h"
 
+using namespace reco;
+
 class TCRecoTauDiscriminationAgainstHadronicJets : public CaloTauDiscriminationProducerBase {
     public:
-      	explicit TCRecoTauDiscriminationAgainstHadronicJets(const ParameterSet& iConfig):CaloTauDiscriminationProducerBase(iConfig){   
+      	explicit TCRecoTauDiscriminationAgainstHadronicJets(const edm::ParameterSet& iConfig):CaloTauDiscriminationProducerBase(iConfig){   
 		tcTauAlgorithm = new TCTauAlgorithm(iConfig);
       	}
       	~TCRecoTauDiscriminationAgainstHadronicJets(){} 
       	double discriminate(const CaloTauRef& theCaloTauRef);
-	void beginEvent(const Event&, const EventSetup&);
+	void beginEvent(const edm::Event&, const edm::EventSetup&);
 
     private:
 	TCTauAlgorithm*  tcTauAlgorithm;
 };
 
-void TCRecoTauDiscriminationAgainstHadronicJets::beginEvent(const Event& iEvent, const EventSetup& iSetup){
+void TCRecoTauDiscriminationAgainstHadronicJets::beginEvent(const edm::Event& iEvent, const edm::EventSetup& iSetup){
 	tcTauAlgorithm->eventSetup(iEvent,iSetup);
 }
 
