26c322801bfea1cfc076dade07521fa6ab753211
cmsbuild <>
2012-01-31 10:10:35 +0000

    This commit was manufactured by cvs2git to create tag 'JetMETCorrections- TauJet-CMSSW_6_0_0_pre11'.
    Sprout from master 2010-09-12 08:34:16 UTC Sami Lehti <sha1-b6a1a8a9c5daac3b0bcd6bc227cf543f53744041@cern.ch> 'Commented function TCTauCorrector::correction(const reco::CaloJet& fJet)'
    Cherrypick from master 2012-01-31 10:09:06 UTC Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4@cern.ch> 'Drop set but unused variable.':
        plugins/TCRecoTauDiscriminationAgainstHadronicJets.cc
        plugins/TCRecoTauDiscriminationAlgoComponent.cc
    Cherrypick from master 2010-10-19 22:22:26 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Remove 'using' directives from headers':
        test/TCTauAnalysis.cpp

--- plugins/TCRecoTauDiscriminationAlgoComponent.cc
+++ plugins/TCRecoTauDiscriminationAlgoComponent.cc
@@ -8,26 +8,28 @@
 #include "RecoTauTag/RecoTau/interface/TauDiscriminationProducerBase.h"
 #include "RecoTauTag/RecoTau/interface/TCTauAlgorithm.h"
 
+using namespace reco;
+
 class TCRecoTauDiscriminationAlgoComponent : public CaloTauDiscriminationProducerBase {
     public:
-      	explicit TCRecoTauDiscriminationAlgoComponent(const ParameterSet& iConfig):CaloTauDiscriminationProducerBase(iConfig){   
+      	explicit TCRecoTauDiscriminationAlgoComponent(const edm::ParameterSet& iConfig):CaloTauDiscriminationProducerBase(iConfig){   
 		tcTauAlgorithm = new TCTauAlgorithm(iConfig);
       	}
       	~TCRecoTauDiscriminationAlgoComponent(){} 
       	double discriminate(const CaloTauRef& theCaloTauRef);
-	void beginEvent(const Event&, const EventSetup&);
+	void beginEvent(const edm::Event&, const edm::EventSetup&);
 
     private:
 	TCTauAlgorithm*  tcTauAlgorithm;
 };
 
-void TCRecoTauDiscriminationAlgoComponent::beginEvent(const Event& iEvent, const EventSetup& iSetup){
+void TCRecoTauDiscriminationAlgoComponent::beginEvent(const edm::Event& iEvent, const edm::EventSetup& iSetup){
 	tcTauAlgorithm->eventSetup(iEvent,iSetup);
 }
 
 
 double TCRecoTauDiscriminationAlgoComponent::discriminate(const CaloTauRef& theCaloTauRef){
-	math::XYZTLorentzVector p4 = tcTauAlgorithm->recalculateEnergy(*theCaloTauRef);
+	tcTauAlgorithm->recalculateEnergy(*theCaloTauRef);
 	return (tcTauAlgorithm->algoComponent());
 }
 
